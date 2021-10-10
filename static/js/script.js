1c39b8f30a411a6afa110ad663e253194c3695c3
qplatt <quincyplatt@gmail.com>
2021-10-10 00:39:08 +0000

    Add plan form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,14 @@
-// Auto initiating materialize elements 
-M.AutoInit();
+// Initializing materialize functions
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+});
